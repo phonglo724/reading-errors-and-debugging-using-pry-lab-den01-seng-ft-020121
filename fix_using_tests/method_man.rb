f1a290a7b0cd638ae7c7b1f416c8473b0657c3da
Phong Lo <phonglo724@gmail.com>
2021-01-14 20:37:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,10 +4,8 @@ require 'pry'
 
 def start_game(player1, player2)
   return "Hello #{player1} & #{player2}"
-  binding.pry
 end
 
 def play_game(player1, player2)
   return "#{player1} is better than #{player2}"
-  binding.pry
 end
